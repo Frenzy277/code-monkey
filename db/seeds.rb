00906bc7b9d04007d4e747026f0a54dac72bcee2
Frenzy277 <tomas.tomecek@hotmail.com>
2014-12-05 17:20:55 +0100

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-User.find_or_create(first_name: "Tom", last_name: "Tom", email: "tom@e.com", password: "password")
-Fabricate.times(4, :user)
+Fabricate(:user, first_name: "Tom", last_name: "Tom", email: "tom@email.com", password: "password")
+4.times { Fabricate(:user) }
 
 %w(HTML CSS Angular.js Ruby Rails jQuery SQL Java Python).each do |language|
-  Language.find_or_create(name: language, image_url: "#{language.downcase}.jpg")
+  Language.find_or_create_by(name: language, image_url: "#{language.downcase}.jpg")
 end

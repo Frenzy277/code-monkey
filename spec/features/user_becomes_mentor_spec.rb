f688da1ff98b8d245bac4bf03116ceceba957f65
Frenzy277 <tomas.tomecek@hotmail.com>
2014-12-06 21:46:47 +0100

    feature user becomes a mentor spec

--- spec/features/user_becomes_mentor_spec.rb
+++ spec/features/user_becomes_mentor_spec.rb
@@ -1,19 +1,44 @@
 require 'spec_helper'
 
 feature "user wants to be a mentor" do
+  given!(:alice) { Fabricate(:user) }
+  given!(:rails) { Fabricate(:language) }
+
   scenario "becomes the mentor" do
-    alice = Fabricate(:user)
-    rails = Fabricate(:language)
     sign_in(alice)
     click_on_language(rails)
 
-    expect(page).to have_content "#{rails.name} Mentors"
+    expect_to_see("#{rails.name} Mentors")
 
     click_on "Become a Mentor"
-    expect(current_path).to eq(new_skill_path)
+    expect_to_see("Mentor next generation")
+
+    fill_in_and_submit_form(rails)
+
+    verify_flash_message
+    verify_dashboard_path
   end
 end
 
 def click_on_language(language)
   find(:xpath, "//a[@href='/languages/#{language.id}']").click
+end
+
+def expect_to_see(content)
+  expect(page).to have_content content
+end
+
+def fill_in_and_submit_form(language)
+  select(language.name, from: "Skill")
+  fill_in "Description", with: Faker::Lorem.paragraph(2)
+  select("2010", from: "Experience")
+  click_button "Sign the pledge"
+end
+
+def verify_flash_message
+  expect(page).to have_content "Congratulations, you became a new mentor"
+end
+
+def verify_dashboard_path
+  expect(current_path).to eq(dashboard_path)
 end
